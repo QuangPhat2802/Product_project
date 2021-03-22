f36547f4008d3647baf5513e82ffcaf2876997ad
phat <quangphat1111@gmail.com>
2021-03-22 17:36:26 +0700

    config swagger and security

--- src/main/java/com/demo/serviceimpl/ProductServiceImpl.java
+++ src/main/java/com/demo/serviceimpl/ProductServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,5 +59,4 @@ public class ProductServiceImpl implements ProductService {
 	public ProductEntity findByProductId(int productId) {
 		return productRepo.findByProductId(productId);
 	}
-
 }
